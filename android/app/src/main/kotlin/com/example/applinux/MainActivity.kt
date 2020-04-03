42083360f3d33772afc3ab5b3ed1a07794f7b812
Lunas Julias <juliasouild@gmail.com>
2020-04-03 16:19:08 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/applinux/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.applinux
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+/// Flutter App for Desktop = Mobile!!
+// What is Flutter?
+/// Do you know what is an Library / Lib?
+// APP or Application
+// App to computer = 
+
+// 
+
+main(){
+  thanos(); // 
+}
+
+nome(){ }
